594cb79fa61b2c1884e8aea0711b28dfbe1cf290
Josh <jpdunn03@gmail.com>
2019-06-18 11:41:19 +1000

    Updated example to actually translate text.

--- angular-translation-experimentation/src/app/app.component.ts
+++ angular-translation-experimentation/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+
+  public componentLabel: string;
+  
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+
+    translate.stream('HOME.TEST').subscribe((x) => this.componentLabel = x);
+  }
   
 }
--- angular-translation-experimentation/src/app/app.module.ts
+++ angular-translation-experimentation/src/app/app.module.ts
@@ -5,9 +5,34 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent
+  ],
+  bootstrap: [
+     AppComponent 
+  ]
 })
 export class AppModule { }
